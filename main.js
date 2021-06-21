f27ea08892dfa8fb608c6be6661d0db3c795d3b4
zakarieOmar <86239637+zakarieOmar@users.noreply.github.com>
2021-06-21 16:49:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll("button");
+const display = document.querySelector(".display");
+
+//add event listener to each botton
+buttons.forEach(function (button) {
+  button.addEventListener("click", calculate);
+});
+
+function calculate(event) {
+  const clickedButtonValue = event.target.value;
+
+  if (clickedButtonValue === "=") {
+    if (display.value !== "") {
+      display.value = eval(display.value);
+    }
+  } else if (clickedButtonValue === "C") {
+    display.value = "";
+  } else {
+    display.value += clickedButtonValue;
+  }
+}
